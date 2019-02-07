a0e9dfe2572863d07267c3f4ce4c66711ea9d78c
nwill30 <techexpressnw@gmail.com>
2019-02-07 17:33:03 -0600

    Initial commit

---
+++ src/app/pages/tabs/tabs.router.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+ 
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'films',
+        children: [
+          {
+            path: '',
+            loadChildren: '../films/films.module#FilmsPageModule'
+          },
+          {
+            /**:id helps to pass info from url */
+            path: ':id',
+            loadChildren: '../film-details/film-details.module#FilmDetailsPageModule'
+          }
+        ]
+      },
+      {
+        path: 'people',
+        children: [
+          {
+            path: '',
+            loadChildren: '../people/people.module#PeoplePageModule'
+          }
+        ]
+      },
+      {
+        path: 'planets',
+        children: [
+          {
+            path: '',
+            loadChildren: '../planets/planets.module#PlanetsPageModule'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/films',
+    pathMatch: 'full'
+  }
+];
+ 
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsRouterModule { }
